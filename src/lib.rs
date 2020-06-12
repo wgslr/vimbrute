a6356e948daccd738084842ae923ec16e903a4a1
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-06-12 02:31:58 +0200

    Read possible password from stdin

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::error;
 use std::fmt;
 use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::string;
 
 pub mod cli;
 pub mod crypto;
@@ -20,8 +23,10 @@ impl error::Error for BadInputFile {}
 
 pub fn run(params: cli::Params) -> Result<()> {
     let file_data = fs::read(&params.file)?;
+    let header = &file_data[0..12];
+    let encrypted_data = &file_data[12..];
 
-    match &file_data[0..12] {
+    match header {
         b"VimCrypt~03!" => (),
         _ => {
             eprintln!("Provided file does not contain data encrypted using VimCrypt03 method.");
@@ -29,9 +34,27 @@ pub fn run(params: cli::Params) -> Result<()> {
         }
     }
 
-
-    let result = crypto::blowfish2_decrypt(&file_data[12..], "12345678912345");
-    print!("{}", String::from_utf8(result).unwrap());
+    for line in io::stdin().lock().lines() {
+        match line {
+            Ok(password) => {
+                if attempt_decrypt(&encrypted_data, &password) {
+                    println!("{}", &password)
+                }
+            }
+            Err(_) => break
+        }
+    }
 
     Ok(())
+}
+
+// Tries to decrypt the file using given password.
+// If the obtained plaintext is valid UTF8, the operation
+// is considered succesful.
+fn attempt_decrypt(encrypted_data: &[u8], password: &str) -> bool {
+    let result = crypto::blowfish2_decrypt(&encrypted_data, password);
+    match String::from_utf8(result) {
+        Ok(_) => true,
+        Err(string::FromUtf8Error { .. }) => false,
+    }
 }
