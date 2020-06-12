2301a8d1ad503256e03f311d346856b2d0182b65
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-06-12 14:27:21 +0200

    Add threads number as a CLI argument

--- src/cli.rs
+++ src/cli.rs
@@ -6,6 +6,9 @@ use structopt::StructOpt;
 pub struct Params {
     #[structopt(short, long)]
     pub file: PathBuf,
+
+    #[structopt(short, long, default_value = "1")]
+    pub threads: u8,
 }
 
 #[cfg(test)]
@@ -16,12 +19,26 @@ mod test {
 
     #[test]
     fn parse_good_args() {
-        let args: Vec<String> = vec!["program".to_string(), "-f".to_string(), "file".to_string()];
+        let args: Vec<String> = vec![
+            "program".to_string(),
+            "-f".to_string(),
+            "file".to_string(),
+            "-t".to_string(),
+            "3".to_string(),
+        ];
         assert_eq!(
             Params {
                 file: PathBuf::from("file"),
+                threads: 3
             },
-            Params::from_iter(args.into_iter())
-        )
+            Params::from_iter(args[..].iter())
+        );
+        assert_eq!(
+            Params {
+                file: PathBuf::from("file"),
+                threads: 1
+            },
+            Params::from_iter(args[..3].iter())
+        );
     }
 }
